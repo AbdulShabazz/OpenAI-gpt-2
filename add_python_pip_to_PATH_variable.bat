3f9a8354e1aa2b2b120d480f5ec3ee1f1e283df7
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-09-07 05:58:16 -0400

    Add files via upload

--- add_python_pip_to_PATH_variable.bat
+++ add_python_pip_to_PATH_variable.bat
@@ -1,3 +1,4 @@
-REM Add pip to PATH variable
+REM Add pip to PATH variable (VSCode)
 setx PATH "%PATH%;C:\Users\WDAGUtilityAccount\AppData\Local\Programs\Python\Python312\Scripts"
+REM You may need to restart your terminal editor and or computer for changes to take effect
 pip --version
