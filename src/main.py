f583a1c16323f52437f84d8eecdcaa45ca662aaf
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-09-09 04:27:16 -0400

    Update main.py (Add Encode / Decode support to the Model)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
 """test case encoding/decoding utility"""
 
-import encoder as gpt
+import sample
+import model
+import encoder
 
-gpt2 = gpt.get_encoder('124M','../models')
+# Define the prompt
+prompt = "Hello, how are you today?"
+
+gpt2 = encoder.get_encoder('124M','../models')
+
+tokens = gpt2.encode(prompt) # [15496, 11, 703, 389, 345, 1909, 30]
+
+# Generate a response (optional)
+hparams = model.default_hparams()
+output = sample.sample_sequence(hparams,tokens, length=50)
+
+# Decode the generated output
+generated_text = gpt2.decode(output[0], skip_special_tokens=True)
+print("Generated text:", generated_text)
