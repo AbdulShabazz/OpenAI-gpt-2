50e50a46afbf5bee0e2145f695b2a78b9a6f38c9
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-09-10 06:24:55 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import gpt2
 
 model_name = '124M'
 model_dir = '../models'
+
 prompt = "Hello, how are you today?"
 
 text_completion = gpt2.sample_sequence(model_name=model_name, model_dir=model_dir, context=prompt, length=50)
