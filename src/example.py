dd9e956e6526b7a56de65d74bdea7f66bdfebda6
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-09-11 00:45:29 -0400

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,12 +1,12 @@
 """test gpt2 model"""
 
-import gpt2
+import gpt
 
 model_name = '124M'
 model_dir = '../models'
 
 prompt = "Hello, how are you today?"
 
-text_completion = gpt2.sample_sequence(model_name=model_name, model_dir=model_dir, context=prompt, length=50)
+text_completion = gpt.sample_sequence(model_name=model_name, model_dir=model_dir, context=prompt, length=50)
 
 print("Generated text:", text_completion)
