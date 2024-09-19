568f1ba250d66c3986ae720cc6dcaa2e80b2d971
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-09-19 11:48:36 -0400

    Add modules_to_import.py

---
+++ modules_to_import.py
@@ -0,0 +1,12 @@
+
+modules_to_import = [
+    ('fire', 'fire'),
+    ('regex', 're'),
+    ('aiohttp', 'aiohttp'),
+    ('aiofiles', 'aiofiles'),
+    ('asyncio', 'asyncio'),
+    ('requests', 'requests'),
+    ('tqdm', 'tqdm', 'from tqdm import tqdm'),
+    ('tensorflow', 'tf'),
+    ('torch', 'pytorch')
+]
